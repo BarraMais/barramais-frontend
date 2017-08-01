060387a3e3eee71a376220bd71f84f3abc83f627
julio.gomes <julio.gomes@clicksoft.com.br>
2017-08-01 10:49:19 -0300

    a

--- src/pages/user-popover/user-popover.ts
+++ src/pages/user-popover/user-popover.ts
@@ -9,7 +9,7 @@ import { JwtHelper } from 'angular2-jwt';
 
   selector: 'page-user-popover',
   template: `
-      <p (click)="unfriend(user)">EXCLUIR AMIGO</p>
+      <p (click)="unfriend()">EXCLUIR AMIGO</p>
       <p (click)="close()">BLOQUEAR</p>
       <p (click)="close()">NÃO VER PUBLICAÇÕES</p>
   `
